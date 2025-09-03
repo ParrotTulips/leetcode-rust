9152356da198983c81199a5e16ebb9b18333533f
Shawn Feng <cscfgogogo@gmail.com>
2025-09-03 20:31:41 +0800

    feat: practice 0001

--- src/solution/leetcode0001_two_sum.rs
+++ src/solution/leetcode0001_two_sum.rs
@@ -10,20 +10,6 @@ impl Solution {
                 None => m.insert(*v, i as i32),
             };
         }
-        vec![]
-    }
-    pub fn two_sum_bubble(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let length = nums.len();
-
-        for gap in 1..length {
-            for right in gap..length {
-                let left = right - gap;
-                if nums[left] + nums[right] == target {
-                    return vec![left as i32, right as i32];
-                }
-            }
-        }
-
-        vec![] // Return an empty vector if no solution found
+        return vec![];
     }
 }
